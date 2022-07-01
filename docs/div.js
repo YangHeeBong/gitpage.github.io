e2a79efd2f26c463a207f2a38328c99a137ca18e
ggii9911 <ggii9911@nate.com>
2022-07-01 10:20:17 +0900

    update

--- docs/div.js
+++ docs/div.js
@@ -1,4 +1,8 @@
 function init() {
+	console.log("init start");
+	
 	Module.Start(window.innerWidth, window.innerHeight - 200);
 	Module.getViewCamera().setLocation(new Module.JSVector3D(129.13782351349964, 35.17887462859219, 8000));
+
+	console.log("init end");
 }
