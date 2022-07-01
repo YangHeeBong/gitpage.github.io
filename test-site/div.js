438fe59f2af29b014ecbc37d01492983893e2689
ggii9911 <ggii9911@nate.com>
2022-07-01 13:22:50 +0900

    10

---
+++ test-site/div.js
@@ -0,0 +1,10 @@
+function init() {
+	console.log("init start");
+
+	Module.Start(window.innerWidth, window.innerHeight - 200);
+	Module.getViewCamera().setLocation(new Module.JSVector3D(129.13782351349964, 35.17887462859219, 8000));
+
+	ㄷㅅㄴㄷㅅ
+	console.log("init end");
+	console.log("메에에");
+}
