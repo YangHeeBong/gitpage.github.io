74cfbd0fdc932479a668e052b16b86ed8409840c
ggii9911 <ggii9911@nate.com>
2022-07-01 10:22:22 +0900

    fail test

--- docs/div.js
+++ docs/div.js
@@ -4,6 +4,7 @@ function init() {
 	Module.Start(window.innerWidth, window.innerHeight - 200);
 	Module.getViewCamera().setLocation(new Module.JSVector3D(129.13782351349964, 35.17887462859219, 8000));
 
+	ㄷㅅㄴㄷㅅ
 	console.log("init end");
 	console.log("메에에");
 }
